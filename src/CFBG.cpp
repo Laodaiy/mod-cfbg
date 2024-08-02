f35bfee9690fa77bb0fbefc15148e829be201330
duangon <ioridjh2002@163.com>
2024-08-03 00:24:19 +0800

    fix: remove SendWorldTextOptional

--- src/CFBG.cpp
+++ src/CFBG.cpp
@@ -944,10 +944,10 @@ void CFBG::SendMessageQueue(BattlegroundQueue* bgQueue, Battleground* bg, PvPDif
                     }
                 }
             }
-            else
-            {
-                sWorld->SendWorldTextOptional(LANG_BG_QUEUE_ANNOUNCE_WORLD, ANNOUNCER_FLAG_DISABLE_BG_QUEUE, bgName.c_str(), q_min_level, q_max_level, qTotal, MinPlayers);
-            }
+            //else
+            //{
+            //    sWorld->SendWorldTextOptional(LANG_BG_QUEUE_ANNOUNCE_WORLD, ANNOUNCER_FLAG_DISABLE_BG_QUEUE, bgName.c_str(), q_min_level, q_max_level, qTotal, MinPlayers);
+            //}
         }
     }
 }
